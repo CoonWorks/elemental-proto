ed52938e461eed3f3076f12e2b76138ee49b28ac
CoonWorks <57781384+CoonWorks@users.noreply.github.com>
2019-12-01 21:04:04 -0300

    Update elemental.js

--- elemental.js
+++ elemental.js
@@ -11,18 +11,81 @@ function cerrarCaja(){
 }
 
 function cambioMetal(){
-  document.contenedor.getElementsByClassName("alcalino").style.backgroundColor = "rgb(255,0,0)";
-  document.contenedor.getElementsByClassName("terreo").style.backgroundColor = "rgb(255,0,0)";
-  document.contenedor.getElementsByClassName("transicion").style.backgroundColor = "rgb(255,0,0)";
-  document.contenedor.getElementsByClassName("otrometal").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e3").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e4").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e11").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e12").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e13").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e19").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e20").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e21").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e22").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e23").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e24").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e25").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e26").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e27").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e28").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e29").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e30").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e31").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e37").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e38").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e39").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e40").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e41").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e42").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e43").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e44").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e45").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e46").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e47").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e48").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e49").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e50").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e55").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e56").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("elem-a").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e72").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e73").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e74").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e75").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e76").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e77").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e78").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e79").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e80").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e81").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e82").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e83").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e84").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e87").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e88").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("elem-b").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e104").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e105").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e106").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e107").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e108").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e112").style.backgroundColor = "rgb(255,0,0)";
 }
 
 function cambioAlc(){
-  document.contenedor.getElementsByClassName("alcalino").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e3").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e11").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e19").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e37").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e55").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e87").style.backgroundColor = "rgb(255,0,0)";
 }
 
 function cambioTer(){
-  document.contenedor.getElementsByClassName("terreo").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e4").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e12").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e20").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e38").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e56").style.backgroundColor = "rgb(255,0,0)";
+  document.getElementById("e88").style.backgroundColor = "rgb(255,0,0)";
 }
 
 function cambioTran(){
